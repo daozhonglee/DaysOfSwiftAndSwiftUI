d4d8e6154521fda3425765f95442c1d5c9ea8086
tianfav <tianfav@163.com>
2025-02-14 01:31:26 +0800

    增加 15.VideoSplash 在视频上叠加其他视图元素

---
+++ 16.TumblrMenu/16.TumblrMenu/_6_TumblrMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_TumblrMenuApp.swift
+//  16.TumblrMenu
+//
+//  Created by shanquan on 2025/2/14.
+//
+
+import SwiftUI
+
+@main
+struct _6_TumblrMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
