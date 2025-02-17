592c72f9cb7bd759f218a3fa1b59bd2c62844b14
tianfawei <tianfawei@email.com>
2025-02-17 12:14:04 +0800

    新增 21.QuickActions 只占位,稍后实现

---
+++ 21.QuickActions/21.QuickActions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  21.QuickActions
+//
+//  Created by zhihu on 17/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 21.QuickActions/21.QuickActions/_1_QuickActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_QuickActionsApp.swift
+//  21.QuickActions
+//
+//  Created by zhihu on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct _1_QuickActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
